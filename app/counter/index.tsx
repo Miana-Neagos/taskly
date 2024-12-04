8ade73601131e822a8e63bc19c1df50f147269c0
Miana Neagos <miananeagos@gmail.com>
2024-12-04 20:51:07 +0200

    Add countdown timer, persist countdown state and late task completion notification.

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -2,22 +2,77 @@ import { View, Text, StyleSheet, TouchableOpacity, Alert } from "react-native";
 import { theme } from "../../theme";
 import { registerForPushNotificationsAsync } from "../../utils/registerForPushNotificationsAsync";
 import * as Notifications from "expo-notifications";
+import { useEffect, useState } from "react";
+import { intervalToDuration, isBefore } from "date-fns";
+import TimeSegment from "../../components/TimeSegment";
+import { getFromStorage, saveToStorage } from "../../utils/storage";
+
+// hard coding a possible frequency for the task -10 secs
+const frequency = 10 * 1000;
+const counterStorageKey = "taskly-counter";
+
+type PersistedCountdownState = {
+  currentNotifId: string | undefined;
+  completedAtTimestamp: number[];
+};
+
+type CountdownStatus = {
+  isOverdue: boolean;
+  distance: ReturnType<typeof intervalToDuration>;
+};
 
 export default function CounterScreen() {
+  // const [secondsElapsed, setSecondsElapsed] = useState(0);
+  const [countdownState, setCountdownState] = useState<PersistedCountdownState>();
+  const [status, setStatus] = useState<CountdownStatus>({
+    isOverdue: false,
+    distance: {},
+  });
+
+  /*------- Keeps track of the most recent time the task was completed (the only task) ----------*/
+  const lastCompletedTimestamp = countdownState?.completedAtTimestamp[0];
+
+  /* ------ Fetching the countdown state from storage ----------*/
+  useEffect(() => {
+    const initialCountdownState = async () => {
+      const value = await getFromStorage(counterStorageKey);
+      setCountdownState(value);
+    };
+    initialCountdownState();
+  }, []);
+
+  useEffect(() => {
+    // setInterval inside the useEffect ensures that the countdown continuously updates every second, regardless of whether "lastCompletedTimestamp" changes
+    const intervalId = setInterval(() => {
+      // calculating the due time
+      const timestamp = lastCompletedTimestamp ? lastCompletedTimestamp + frequency : Date.now();
+
+      const isOverdue = isBefore(timestamp, Date.now());
+      const distance = intervalToDuration(
+        isOverdue
+          ? { start: timestamp, end: Date.now() }
+          : { start: Date.now(), end: timestamp }
+      );
+      setStatus({ isOverdue, distance });
+    }, 1000);
+    return () => {
+      clearInterval(intervalId);
+    };
+    // passing a dependancy for useEffect so that the countdown logic is recalculated each time "lastUpdatedTimestamp" is modified
+  }, [lastCompletedTimestamp]);
+
   const scheduleNotifications = async () => {
+    let pushNotifId;
     const result = await registerForPushNotificationsAsync();
-    console.log("this is SCHEDULE NOTIFICATION");
-    console.log(result);
-    
 
     if (result === "granted") {
-      await Notifications.scheduleNotificationAsync({
+      pushNotifId = await Notifications.scheduleNotificationAsync({
         content: {
-          title: "Time is up ⌛",
-          body: "Get Moving",
+          title: "Time is up ⌛, task is overdue",
+          // body: "Get Moving",
         },
         trigger: {
-          seconds: 5,
+          seconds: frequency / 1000,
         },
       });
     } else {
@@ -26,17 +81,64 @@ export default function CounterScreen() {
         "Enable notification permissions for Taskly from device Settings"
       );
     }
+    //check countdown state for existing scheduled notifications, then cancel them
+    if (countdownState?.currentNotifId) {
+      await Notifications.cancelScheduledNotificationAsync(countdownState.currentNotifId);
+    }
+
+    const newCountdownState: PersistedCountdownState = {
+      currentNotifId: pushNotifId,
+      completedAtTimestamp: countdownState
+        ? [Date.now(), ...countdownState.completedAtTimestamp]
+        : [Date.now()],
+    };
+    setCountdownState(newCountdownState);
+    await saveToStorage(counterStorageKey, newCountdownState);
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Counter Screen</Text>
+    <View
+      style={[
+        styles.container,
+        status.isOverdue ? styles.containerLate : undefined,
+      ]}
+    >
+      {status.isOverdue ? (
+        <Text style={[styles.heading, styles.whiteText]}>
+          {" "}
+          Task is overdue by
+        </Text>
+      ) : (
+        <Text style={styles.heading}>Task is due in...</Text>
+      )}
+      <View style={styles.timeSegmentView}>
+        <TimeSegment
+          unit="Days"
+          number={status.distance.days ?? 0}
+          textStyle={status.isOverdue ? styles.whiteText : undefined}
+        ></TimeSegment>
+        <TimeSegment
+          unit="Hours"
+          number={status.distance.hours ?? 0}
+          textStyle={status.isOverdue ? styles.whiteText : undefined}
+        ></TimeSegment>
+        <TimeSegment
+          unit="Minutes"
+          number={status.distance.minutes ?? 0}
+          textStyle={status.isOverdue ? styles.whiteText : undefined}
+        ></TimeSegment>
+        <TimeSegment
+          unit="Seconds"
+          number={status.distance.seconds ?? 0}
+          textStyle={status.isOverdue ? styles.whiteText : undefined}
+        ></TimeSegment>
+      </View>
       <TouchableOpacity
         style={styles.button}
         activeOpacity={0.8}
         onPress={scheduleNotifications}
       >
-        <Text style={styles.buttonText}>Schedule Notification</Text>
+        <Text style={styles.buttonText}>Complete Task</Text>
       </TouchableOpacity>
     </View>
   );
@@ -63,5 +165,22 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 24,
+    color: theme.colorBlack,
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 18,
+  },
+  timeSegmentView: {
+    flexDirection: "row",
+    fontWeight: "bold",
+    marginBottom: 18,
+  },
+  containerLate: {
+    backgroundColor: theme.colorOrange,
+  },
+  whiteText: {
+    color: theme.colorWhite,
   },
 });
--- utils/orderShoppingList.ts
+++ utils/orderShoppingList.ts
@@ -1,3 +1,16 @@
+import { ShoppingListItemType } from "../app";
+
+export function orderShoppingList(shoppingList: ShoppingListItemType[]):ShoppingListItemType[] {
+  return shoppingList.sort((item1, item2) => {
+    if (!item1.completedAtTimestamp && item2.completedAtTimestamp) return -1;
+    if (item1.completedAtTimestamp && !item2.completedAtTimestamp) return 1;
+    return (item2.completedAtTimestamp || item2.lastUpdatedTimestamp) -
+    (item1.completedAtTimestamp || item1.lastUpdatedTimestamp);
+  });
+}
+
+/*------------ Course version --------------------*/
+
 // export function orderShoppingList(shoppingList) {
 //   return shoppingList.sort((item1, item2) => {
 //     if (item1.completedAtTimestamp && item2.completedAtTimestamp) {
@@ -19,13 +32,3 @@
 //     return 0;
 //   });
 // }
-import { ShoppingListItemType } from "../app";
-
-export function orderShoppingList(shoppingList: ShoppingListItemType[]):ShoppingListItemType[] {
-  return shoppingList.sort((item1, item2) => {
-    if (!item1.completedAtTimestamp && item2.completedAtTimestamp) return -1;
-    if (item1.completedAtTimestamp && !item2.completedAtTimestamp) return 1;
-    return (item2.completedAtTimestamp || item2.lastUpdatedTimestamp) -
-           (item1.completedAtTimestamp || item1.lastUpdatedTimestamp);
-  });
-}
